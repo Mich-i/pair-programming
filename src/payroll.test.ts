14482a154a2c5f319e34595d7df1cb9d75aeead7
Michael Stutz <michael_stutz@sluz.ch>
2025-05-08 20:33:49 +0200

    Testfall 16 Jähriger

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -1,1 +1,17 @@
-hallo m
+import { calculatePayslip, Salary } from "./payroll";
+
+describe('calculatePayslip', () => {
+    it('16 Jahriger mit 700. keine Abzüge', () => {
+        const salary: Salary = {
+            born: new Date('2009-05-01'),
+            payday: new Date('2025-05-01'),
+            gross: 700,
+        };
+
+        const playslip = calculatePayslip(salary);
+
+        expect(playslip.totalDeductions).toBe(0);
+        expect(playslip.net).toBe(700);
+        expect(playslip.deductions).toBe(0);
+    })
+})
